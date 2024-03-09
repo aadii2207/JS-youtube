86549e265aad5eeca01cf9551f634099f398d180
aadii2207 <119290765+aadii2207@users.noreply.github.com>
2024-03-09 18:20:41 +0000

    converiosn chnage

---
+++ 01_basics/alpha.js
@@ -0,0 +1,10 @@
+let score=undefined;
+// console.log(typeof score)
+
+let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+let alpha=a23;
+let str=String(alpha);
+console.log(str)
---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const pass=12345;
+let email="aditya713@gmail.com"
+var accountPass="1234321"
+city="meerut"
+let state;
+state='adity'
+
+console.table([pass,email,accountPass,city,state])
+console.log(typeof 1798,"cjl")
+console.log("fdlkskdjs")
